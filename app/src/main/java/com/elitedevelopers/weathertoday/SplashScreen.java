7169c51d4b853b2e0b488e946a927c6c912acc2b
Monir Hossain <mknishad1@ymail.com>
2016-08-04 01:05:04 +0600

    add splash screen

---
+++ app/src/main/java/com/elitedevelopers/weathertoday/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.elitedevelopers.weathertoday;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Nishad on 04-Aug-16.
+ */
+public class SplashScreen extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000; // set the delay for category screen
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash_screen);
+
+        /* New Handler to start the LoginActivity-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the HomeActivity. */
+                Intent mainIntent = new Intent(SplashScreen.this, HomeActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+}
