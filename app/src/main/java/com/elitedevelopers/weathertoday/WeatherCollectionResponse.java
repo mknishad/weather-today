dd400b0afa73f77e548f0769f7146a90e89a12fa
ASIF-006 <Professional100001@yahoo.com>
2016-08-06 11:59:31 +0600

    add WeatherCollectionResponse class

---
+++ app/src/main/java/com/elitedevelopers/weathertoday/WeatherCollectionResponse.java
@@ -0,0 +1,138 @@
+package com.elitedevelopers.weathertoday;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Mobile App Develop on 6-8-16.
+ */
+public class WeatherCollectionResponse {
+
+    @SerializedName("city")
+    @Expose
+    private City city;
+    @SerializedName("time")
+    @Expose
+    private Integer time;
+    @SerializedName("main")
+    @Expose
+    private Main main;
+    @SerializedName("wind")
+    @Expose
+    private Wind wind;
+    @SerializedName("clouds")
+    @Expose
+    private Clouds clouds;
+    @SerializedName("weather")
+    @Expose
+    private List<Weather> weather = new ArrayList<Weather>();
+
+    /**
+     *
+     * @return
+     * The city
+     */
+    public City getCity() {
+        return city;
+    }
+
+    /**
+     *
+     * @param city
+     * The city
+     */
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    /**
+     *
+     * @return
+     * The time
+     */
+    public Integer getTime() {
+        return time;
+    }
+
+    /**
+     *
+     * @param time
+     * The time
+     */
+    public void setTime(Integer time) {
+        this.time = time;
+    }
+
+    /**
+     *
+     * @return
+     * The main
+     */
+    public Main getMain() {
+        return main;
+    }
+
+    /**
+     *
+     * @param main
+     * The main
+     */
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    /**
+     *
+     * @return
+     * The wind
+     */
+    public Wind getWind() {
+        return wind;
+    }
+
+    /**
+     *
+     * @param wind
+     * The wind
+     */
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    /**
+     *
+     * @return
+     * The clouds
+     */
+    public Clouds getClouds() {
+        return clouds;
+    }
+
+    /**
+     *
+     * @param clouds
+     * The clouds
+     */
+    public void setClouds(Clouds clouds) {
+        this.clouds = clouds;
+    }
+
+    /**
+     *
+     * @return
+     * The weather
+     */
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    /**
+     *
+     * @param weather
+     * The weather
+     */
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+
+}
