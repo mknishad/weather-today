cc54c54f595debd47ba9894631a1a93db227e63f
ASIF-006 <Professional100001@yahoo.com>
2016-08-06 12:03:42 +0600

    add Clouds class

---
+++ app/src/main/java/com/elitedevelopers/weathertoday/Clouds.java
@@ -0,0 +1,33 @@
+package com.elitedevelopers.weathertoday;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Mobile App Develop on 6-8-16.
+ */
+public class Clouds {
+
+    @SerializedName("all")
+    @Expose
+    private Integer all;
+
+    /**
+     *
+     * @return
+     * The all
+     */
+    public Integer getAll() {
+        return all;
+    }
+
+    /**
+     *
+     * @param all
+     * The all
+     */
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+
+}
