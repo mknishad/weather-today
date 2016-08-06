78245dded9a52855c6fcf7eb253b42cf48913a91
ASIF-006 <Professional100001@yahoo.com>
2016-08-06 12:01:20 +0600

    add Cityclass

---
+++ app/src/main/java/com/elitedevelopers/weathertoday/City.java
@@ -0,0 +1,138 @@
+package com.elitedevelopers.weathertoday;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Mobile App Develop on 6-8-16.
+ */
+public class City {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("findname")
+    @Expose
+    private String findname;
+    @SerializedName("country")
+    @Expose
+    private String country;
+    @SerializedName("coord")
+    @Expose
+    private Coord coord;
+    @SerializedName("zoom")
+    @Expose
+    private Integer zoom;
+
+    /**
+     *
+     * @return
+     * The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     * The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     * The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name
+     * The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     * The findname
+     */
+    public String getFindname() {
+        return findname;
+    }
+
+    /**
+     *
+     * @param findname
+     * The findname
+     */
+    public void setFindname(String findname) {
+        this.findname = findname;
+    }
+
+    /**
+     *
+     * @return
+     * The country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     *
+     * @param country
+     * The country
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     *
+     * @return
+     * The coord
+     */
+    public Coord getCoord() {
+        return coord;
+    }
+
+    /**
+     *
+     * @param coord
+     * The coord
+     */
+    public void setCoord(Coord coord) {
+        this.coord = coord;
+    }
+
+    /**
+     *
+     * @return
+     * The zoom
+     */
+    public Integer getZoom() {
+        return zoom;
+    }
+
+    /**
+     *
+     * @param zoom
+     * The zoom
+     */
+    public void setZoom(Integer zoom) {
+        this.zoom = zoom;
+    }
+
+}
